2c813a0911de3cbcaf24bb67aeaf056141ac3561
drincerkini <93202832+drincerkini@users.noreply.github.com>
2023-03-02 16:24:04 +0100

    api project added

---
+++ api/server.js
@@ -0,0 +1,41 @@
+
+import { MONGO_CONECTION_URI, MONGO_DB_NAME, MONGO_PORT, API_PORT} from './config';
+// const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+
+
+mongoose.connect(`${MONGO_CONECTION_URI}:${MONGO_PORT}/${MONGO_DB_NAME}`).then(() => {
+        console.log(`connected to mongodb on port ${MONGO_PORT}`);
+    
+
+        const app = express();
+        
+        app.use(bodyParser.json());
+
+        app.use(cors({
+            origin: '*'
+        }))
+
+        app.get('/health', (req, res) => {
+            res.json({succes : true})
+        });
+
+        app.get('/api', (req, res) => {
+            res.json({succes : true})
+        });
+
+
+        app.get('*', (req, res) => {
+            res.json({msg: "Not Found"});
+        });
+
+        app.listen(API_PORT, () => {
+            console.log(`Server is listening to port ${API_PORT} ....`)
+        })
+
+    }
+);
