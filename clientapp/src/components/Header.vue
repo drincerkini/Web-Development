cef2affc2e761eebaba5169507a75e5d9898ada2
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2023-03-09 14:20:31 +0100

    User auth and some routes fixed

---
+++ clientapp/src/components/Header.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+    <header class="head">
+        <p v-if="$store.state.user">Hello{{ $store.state.user.email }}!</p>
+    </header>
+</template>
+
+
+<script>
+
+export default{
+    data(){
+        return{
+            path:'/', name:'Home'
+        }
+    }
+}
+</script>
--- clientapp/src/store/index.js
+++ clientapp/src/store/index.js
@@ -1,109 +1,106 @@
-import { createStore } from 'vuex';
-import axios from 'axios';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createStore } from "vuex";
+import axios from "axios";
+import signupUser from "../../firebase/user/signupUser";
+import loginUser from "../../firebase/user/loginUser";
 
-const API_URL = 'http://localhost:3000/products';
+const API_URL = "http://localhost:3000/products";
 
 const store = createStore({
-    state: {
-        user: null,
-        products: []
+  state: {
+    user: null,
+    products: [],
+  },
+  getters: {
+    username(state) {
+      return state.user.email;
     },
-    getters: {
-        username(state){
-            return state.user.email;
-        }
+  },
+  mutations: {
+    //Mutation for setting an actual user
+    setUser(state, user) {
+      state.user = user;
     },
-    mutations: {
 
-
-        SET_USER(state, user) {
-            state.user = user;
-        },
-
-        // READ operation
-        getProducts(state, products){
-            state.products = products;
-        },
-
-        // CREATE operation
-        addProduct(state, product){
-            state.products.push(product);
-        },
-
-        // UPDATE operation
-        updateProduct(state, product){
-            const index = state.products.findIndex(i => i._id === product._id);
-            if(index !== -1){
-                state.products.splice(index, 1, product);
-            }
-        },
-
-        // DELETE operation
-        deleteProduct(state, productId){
-            state.products = state.products.filter(prod => prod._id !== productId);
-        }
-
+    // READ operation
+    getProducts(state, products) {
+      state.products = products;
     },
-    actions: {
-        
-        async register( { commit },  user){
-            const response = await createUserWithEmailAndPassword(getAuth(), user.email, user.password);
-            if(response) {
-                commit('SET_USER', response.user);
-            }else{
-                throw new Error('Unable to register user');
-            }
-        },
 
-        async logIn( { commit }, user){
-            const response = await signInWithEmailAndPassword(getAuth(), user.email, user.password);
-            if(response) {
-                commit('SET_USER', response.user);
-            }else {
-                throw new Error('Login failed');
-            }
-        },
+    // CREATE operation
+    addProduct(state, product) {
+      state.products.push(product);
+    },
 
-        async fetchUser(context ,user) {
-            if (user) {
-              context.commit("SET_USER", {
-                email: user.email
-              });
-            } else {
-              context.commit("SET_USER", null);
-            }
-        },
+    // UPDATE operation
+    updateProduct(state, product) {
+      const index = state.products.findIndex((i) => i._id === product._id);
+      if (index !== -1) {
+        state.products.splice(index, 1, product);
+      }
+    },
 
-        async logOut({ commit }){
-            await signOut(getAuth());
-            commit('SET_USER', null);
-        },
+    // DELETE operation
+    deleteProduct(state, productId) {
+      state.products = state.products.filter((prod) => prod._id !== productId);
+    },
+  },
 
-        async getProducts( { commit } ){
-            const response = await axios.get(API_URL);
+  actions: {
+    //REGISTER USER METHOD WHICH CALLS SIGNUPUSER METHOD
+    async register(_, payload) {
+      await signupUser(payload);
+      payload.navigate();
+    },
 
-            commit('getProducts', response.data);
-        },
+    // async login(_, user) {
+    // const response = await signInWithEmailAndPassword(getAuth(), user.email, user.password);
+    // if(response) {
+    //     commit('setUser', response.user);
+    // }else {
+    //     throw new Error('Login failed');
+    // }
+    // },
 
-        async addProduct( { commit }, product){
-            const response = await axios.post(API_URL, product);
+    async login({ commit }, payload) {
+      const { user } = await loginUser(payload);
+      commit('setUser', user);
+      payload.navigate();
+    },
 
-            commit('addProduct', response)
-        },
+    // async fetchUser(context, user) {
+    //   if (user) {
+    //     context.commit("setUser", {
+    //       email: user.email,
+    //     });
+    //   } else {
+    //     context.commit("setUser", null);
+    //   }
+    // },
 
-        async updateProduct( { commit }, product){
-            const response = await axios.put(`${API_URL}/${product._id}`, product);
+    async getProducts({ commit }) {
+      const response = await axios.get(API_URL);
 
-            commit('updateProduct', response.data);
-        },
+      commit("getProducts", response.data);
+    },
 
-        async deleteProduct( { commit }, productId){
-            await axios.delete(`${API_URL}/${productId}`);
+    async addProduct({ commit }, product) {
+      const response = await axios.post(API_URL, product);
 
-            commit('deleteProduct', productId);
-        }
-    }
+      commit("addProduct", response);
+    },
+
+    async updateProduct({ commit }, product) {
+      const response = await axios.put(`${API_URL}/${product._id}`, product);
+
+      commit("updateProduct", response.data);
+    },
+
+    async deleteProduct({ commit }, productId) {
+      await axios.delete(`${API_URL}/${productId}`);
+
+      commit("deleteProduct", productId);
+    },
+  },
 });
 
 export default store;
