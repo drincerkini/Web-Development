11e8562a0fb7b1ed2a352a89453c46708ba9cd65
tringahaliti <th53768@ubt-uni.net>
2023-03-10 19:08:20 +0100

    changed ports

--- clientapp/firebase/db.js
+++ clientapp/firebase/db.js
@@ -2,7 +2,7 @@ import { firebaseApp } from "./index";
 import {getFireStore, connectFirestoreEmulator} from "firebase/firestore";
 
 const db=getFireStore(firebaseApp);
-connectFirestoreEmulator(db,"http://127.0.0.1:8080")
+connectFirestoreEmulator(db,"http://127.0.0.1:8082")
 
 
 export default db;
