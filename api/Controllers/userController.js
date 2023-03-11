5bdca597312362271df89c41b802e78957e76b26
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2023-03-11 21:30:29 +0100

    minor things changed

---
+++ api/Controllers/userController.js
@@ -0,0 +1,12 @@
+// import Joi from 'joi';
+
+// const schema=Joi.object({
+//     name: Joi.string().min(3).max(15).required(),
+//     surname: Joi.string().min(3).max(15).required(),
+//     address: Joi.string().min(5).max(20).required(),
+//     email: Joi.string().min(5).max(20).email().required(),
+//     password: Joi.string().min(8).max(15).alphanum().required(),
+
+// })
+
+// export default schema;
