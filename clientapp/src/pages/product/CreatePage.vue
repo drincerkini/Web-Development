991e7f3dfa564fbd2cce9f037e97e6aa40d601d5
tringahaliti <th53768@ubt-uni.net>
2023-03-20 17:18:54 +0100

    form for product added

--- clientapp/src/pages/product/CreatePage.vue
+++ clientapp/src/pages/product/CreatePage.vue
@@ -1,24 +1,52 @@
 <template>
-    <h3>Create Product</h3>
-
-    <form @submit.prevent="handleAddProduct">
-      <label for="name">Title:</label>
-      <input id="name" v-model="this.title" type="text" />
-  
-      <label for="description">Description:</label>
-      <input id="description" v-model="this.description" type="text" />
-
-      <label for="price">Description:</label>
-      <input id="price" v-model="this.price" type="number" />
-
-      <label for="category">Description:</label>
-      <input id="category" v-model="this.category" type="text" />
-  
-      <label for="image">Image:</label>
-      <input id="image" ref="imageInput" type="file" @change="handleImageChange" />
-  
-      <button type="submit">Submit</button>
-</form>
+<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+<div class="container bootstrap snippets bootdey">
+    <section id="contact" class="gray-bg padding-top-bottom">
+    	<div class="container bootstrap snippets bootdey">
+            <div class="row">
+                <form id="Highlighted-form" class="col-sm-6 col-sm-offset-3" @submit.prevent="handleAddProduct">
+                    <div class="form-group">
+                        <label class="control-label" for="contact-name">Name</label>
+                        <div class="controls">
+                            <input id="title" v-model="this.title" type="text" />
+                        </div>
+                    </div><!-- End name input -->
+                    <div class="form-group">
+                        <label class="control-label" for="contact-name">Description</label>
+                        <div class="controls">
+                            <input id="title" v-model="this.description" type="text" />
+                        </div>
+                    </div><!-- End name input -->
+                    <div class="form-group">
+                        <label class="control-label" for="contact-mail">Price</label>
+                        <div class=" controls">
+                            <input id="description" v-model="this.price" type="text" />
+                        </div>
+                    </div><!-- End email input -->
+                    <div class="form-group">
+                        <label class="control-label" for="contact-mail">Cat</label>
+                        <div class=" controls">
+                            <input id="description" v-model="this.category" type="text" />
+                        </div>
+                    </div><!-- End email input -->
+                    <div class="form-group">
+                        <label class="control-label" for="contact-mail">Cat</label>
+                        <div class=" controls">
+                            <input id="image" ref="imageInput" type="file" @change="handleImageChange" />
+                        </div>
+                    </div><!-- End email input -->
+                    <div class="form-group">
+                        <label class="control-label" for="contact-message">Message</label>
+                        <div class="controls">
+                            <textarea id="contact-message" name="comments" placeholder="Your message" class="form-control requiredField Highlighted-label" rows="6" ></textarea>
+                        </div>
+                    </div><!-- End textarea -->
+                        <button name="submit" type="submit" class="btn btn-info btn-block" >Send Message</button>
+                </form><!-- End Highlighted-form -->
+			</div>	
+		</div>	
+	</section>
+</div>                    
 
 </template>
 
