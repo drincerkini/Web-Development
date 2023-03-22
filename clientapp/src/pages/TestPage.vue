ff5a32c94c089af80427916d6a327c6aba779bf3
tringahaliti <th53768@ubt-uni.net>
2023-03-22 14:08:08 +0100

    Fixed routers

--- clientapp/src/pages/TestPage.vue
+++ clientapp/src/pages/TestPage.vue
@@ -9,11 +9,16 @@
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="panel panel-dark panel-colorful">
                 <div class="panel-body text-center">
-                	<p class="text-uppercase mar-btm text-sm">Visit Today</p>
-                	<i class="fa fa-users fa-5x"></i>
-                	<hr>
-                	<p class="h2 text-thin">254,487</p>
-                	<small><span class="text-semibold">7%</span> Higher than yesterday</small>
+                	  <router-link to="/create-service" class="text-white">
+        			<p class="text-uppercase mar-btm text-sm">ADD SERVICE</p>
+        			<svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-clipboard2-plus-fill" viewBox="0 0 16 16">
+                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z"/>
+                        <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585c.055.156.085.325.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5c0-.175.03-.344.085-.5ZM8.5 6.5V8H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V9H6a.5.5 0 0 1 0-1h1.5V6.5a.5.5 0 0 1 1 0Z"/>
+                    </svg>
+        			<hr>
+        			<p class="h2 text-thin">{{this.numberOfServices}}</p>
+        			<small><span class="text-semibold"><i class="fa fa-unlock-alt fa-fw"></i> {{this.numberOfServices}}</span> Unapproved comments</small>
+                </router-link>
                 </div>
             </div>
         </div>
@@ -36,22 +41,32 @@
         <div class="col-md-3 col-sm-6 col-xs-12">
         	<div class="panel panel-primary panel-colorful">
         		<div class="panel-body text-center">
-        			<p class="text-uppercase mar-btm text-sm">New Order</p>
-        			<i class="fa fa-shopping-cart fa-5x"></i>
+        			  <router-link to="/create-service" class="text-white">
+        			<p class="text-uppercase mar-btm text-sm">ADD SERVICE</p>
+        			<svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-clipboard2-plus-fill" viewBox="0 0 16 16">
+                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z"/>
+                        <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585c.055.156.085.325.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5c0-.175.03-.344.085-.5ZM8.5 6.5V8H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V9H6a.5.5 0 0 1 0-1h1.5V6.5a.5.5 0 0 1 1 0Z"/>
+                    </svg>
         			<hr>
-        			<p class="h2 text-thin">2,423</p>
-        			<small><span class="text-semibold"><i class="fa fa-shopping-cart fa-fw"></i> 954</span> Sales in this month</small>
+        			<p class="h2 text-thin">{{this.numberOfServices}}</p>
+        			<small><span class="text-semibold"><i class="fa fa-unlock-alt fa-fw"></i> {{this.numberOfServices}}</span> Unapproved comments</small>
+                </router-link>
         		</div>
         	</div>
         </div>
         <div class="col-md-3 col-sm-6 col-xs-12">
         	<div class="panel panel-info panel-colorful">
         		<div class="panel-body text-center">
-        			<p class="text-uppercase mar-btm text-sm">Earning</p>
-        			<i class="fa fa-dollar fa-5x"></i>
+          <router-link to="/create-service" class="text-white">
+        			<p class="text-uppercase mar-btm text-sm">ADD SERVICE</p>
+        			<svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-clipboard2-plus-fill" viewBox="0 0 16 16">
+                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z"/>
+                        <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585c.055.156.085.325.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5c0-.175.03-.344.085-.5ZM8.5 6.5V8H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V9H6a.5.5 0 0 1 0-1h1.5V6.5a.5.5 0 0 1 1 0Z"/>
+                    </svg>
         			<hr>
-        			<p class="h2 text-thin">7,428</p>
-        			<small><span class="text-semibold"><i class="fa fa-dollar fa-fw"></i> 22,675</span> Total Earning</small>
+        			<p class="h2 text-thin">{{this.numberOfServices}}</p>
+        			<small><span class="text-semibold"><i class="fa fa-unlock-alt fa-fw"></i> {{this.numberOfServices}}</span> Unapproved comments</small>
+                </router-link>
         		</div>
         	</div>
         </div>        
