866c3f6ebdacbe4c52f5807ffddae1fc13a395aa
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2023-03-22 19:03:26 +0100

    Fixed minor things in forms

--- api/models/ReviewsModel.js
+++ api/models/ReviewsModel.js
@@ -3,12 +3,6 @@ import mongoose, { mongo } from "mongoose";
 const schema = new mongoose.Schema({
     client: String,
     description: String,
-    image: {
-        filename: String,
-        mimetype: String,
-        size: Number,
-        url: String,
-    },
     createdAt: {
         type: Date,
         default: Date.now
--- clientapp/src/pages/ContactPage.vue
+++ clientapp/src/pages/ContactPage.vue
@@ -1,110 +1,102 @@
 <template>
+    <br />
+    <br />
+    <br />
 
-<br />
-<br />
-<br />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+    <div class="container">
+        <div class="contact__wrapper shadow-lg mt-n9">
+            <div>
+                <a href="#" class="d-inline-block text-dark">
+                    <span class="text-muted">Number of contacts </span>
+                    <strong>{{ this.numberOfContacts }}</strong>
+                </a>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-lg-5 contact-info__wrapper gradient-brand-color p-5 order-lg-2">
+                    <h3 class="color--white mb-5">Get in Touch</h3>
 
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
-<div class="container">
-    <div class="contact__wrapper shadow-lg mt-n9">
-       <div>
-        <a href="#" class="d-inline-block text-dark">
-          <span class="text-muted">Number of contacts  </span>
-          <strong>{{ this.numberOfContacts }}</strong>
-        </a>
-      </div>
-        <div class="row no-gutters">
-            <div class="col-lg-5 contact-info__wrapper gradient-brand-color p-5 order-lg-2">
-                <h3 class="color--white mb-5">Get in Touch</h3>
-    
-                <ul class="contact-info__list list-style--none position-relative z-index-101">
-                    <li class="mb-4 pl-4">
-                        <span class="position-absolute"><i class="fas fa-envelope"></i></span> support@bootdey.com
-                    </li>
-                    <li class="mb-4 pl-4">
-                        <span class="position-absolute"><i class="fas fa-phone"></i></span> (021)-241454-545
-                    </li>
-                    <li class="mb-4 pl-4">
-                        <span class="position-absolute"><i class="fas fa-map-marker-alt"></i></span> bootdey Technologies Inc.
-                        <br> 2694 Queen City Rainbow Drive
-                        <br> Florida 99161
-    
-                        <div class="mt-3">
-                            <a href="https://www.google.com/maps" target="_blank" class="text-link link--right-icon text-white">Get directions <i class="link__icon fa fa-directions"></i></a>
+                    <ul class="contact-info__list list-style--none position-relative z-index-101">
+                        <li class="mb-4 pl-4">
+                            <span><i class="fas fa-envelope"></i></span> support@dft.com
+                        </li>
+                        <li class="mb-4 pl-4">
+                            <span><i class="fas fa-phone"></i></span> +38345454454
+                        </li>
+                        <li class="mb-4 pl-4">
+                            <span><i class="fas fa-map-marker-alt"></i></span> DFT Technologies Inc.
+                            <br> Lagjia Kalabria
+                            <br> Prishtinë 10000
+                            <div class="mt-3">
+                                <a href="https://www.google.com/maps" target="_blank"
+                                    class="text-link link--right-icon text-white">Get directions <i
+                                        class="link__icon fa fa-directions"></i></a>
+                            </div>
+                        </li>
+                    </ul>
+
+
+                </div>
+
+                <div class="col-lg-7 contact-form__wrapper p-5 order-lg-1">
+                    <form @submit.prevent="handleAddNews" class="contact-form form-validate">
+                        <div class="row">
+                            <div class="col-sm-6 mb-3">
+                                <div class="form-group">
+                                    <label class="required-field" for="firstName">First Name</label>
+                                    <input type="text" class="form-control" id="name" v-model="this.contact.name"
+                                        placeholder="Wendy">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 mb-3">
+                                <div class="form-group">
+                                    <label for="lastName">Last Name</label>
+                                    <input type="text" class="form-control" id="surname" v-model="this.contact.surname"
+                                        placeholder="Appleseed">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 mb-3">
+                                <div class="form-group">
+                                    <label class="required-field" for="email">Email</label>
+                                    <input type="text" class="form-control" id="email" v-model="this.contact.email"
+                                        placeholder="wendy.apple@seed.com">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 mb-3">
+                                <div class="form-group">
+                                    <label for="phone">Phone Number</label>
+                                    <input type="tel" class="form-control" id="phone" v-model="this.contact.phone"
+                                        placeholder="+383-45-454-545">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12 mb-3">
+                                <div class="form-group">
+                                    <label class="required-field" for="message">How can we help?</label>
+                                    <textarea class="form-control" id="description" v-model="this.contact.description"
+                                        rows="4" placeholder="Hi there, I would like to....."></textarea>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12 mb-3">
+                                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                            </div>
+
                         </div>
-                    </li>
-                </ul>
-    
-                <figure class="figure position-absolute m-0 opacity-06 z-index-100" style="bottom:0; right: 10px">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="444px" height="626px">
-                        <defs>
-                            <linearGradient id="PSgrad_1" x1="0%" x2="81.915%" y1="57.358%" y2="0%">
-                                <stop offset="0%" stop-color="rgb(255,255,255)" stop-opacity="1"></stop>
-                                <stop offset="100%" stop-color="rgb(0,54,207)" stop-opacity="0"></stop>
-                            </linearGradient>
-    
-                        </defs>
-                        <path fill-rule="evenodd" opacity="0.302" fill="rgb(72, 155, 248)" d="M816.210,-41.714 L968.999,111.158 L-197.210,1277.998 L-349.998,1125.127 L816.210,-41.714 Z"></path>
-                        <path fill="url(#PSgrad_1)" d="M816.210,-41.714 L968.999,111.158 L-197.210,1277.998 L-349.998,1125.127 L816.210,-41.714 Z"></path>
-                    </svg>
-                </figure>
+                    </form>
+                </div>
+                <!-- End Contact Form Wrapper -->
+
             </div>
-    
-            <div class="col-lg-7 contact-form__wrapper p-5 order-lg-1">
-                <form @submit.prevent="handleAddNews" class="contact-form form-validate" >
-                    <div class="row">
-                        <div class="col-sm-6 mb-3">
-                            <div class="form-group">
-                                <label class="required-field" for="firstName">First Name</label>
-                                <input type="text" class="form-control" id="name" v-model="this.contact.name" placeholder="Wendy">
-                            </div>
-                        </div>
-    
-                        <div class="col-sm-6 mb-3">
-                            <div class="form-group">
-                                <label for="lastName">Last Name</label>
-                                <input type="text" class="form-control" id="surname" v-model="this.contact.surname" placeholder="Appleseed">
-                            </div>
-                        </div>
-    
-                        <div class="col-sm-6 mb-3">
-                            <div class="form-group">
-                                <label class="required-field" for="email">Email</label>
-                                <input type="text" class="form-control" id="email" v-model="this.contact.email" placeholder="wendy.apple@seed.com">
-                            </div>
-                        </div>
-    
-                        <div class="col-sm-6 mb-3">
-                            <div class="form-group">
-                                <label for="phone">Phone Number</label>
-                                <input type="tel" class="form-control" id="phone" v-model="this.contact.phone" placeholder="(021)-454-545">
-                            </div>
-                        </div>
-    
-                        <div class="col-sm-12 mb-3">
-                            <div class="form-group">
-                                <label class="required-field" for="message">How can we help?</label>
-                                <textarea class="form-control" id="description" v-model="this.contact.description" rows="4" placeholder="Hi there, I would like to....."></textarea>
-                            </div>
-                        </div>
-    
-                        <div class="col-sm-12 mb-3">
-                            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-                        </div>
-    
-                    </div>
-                </form>
-            </div>
-            <!-- End Contact Form Wrapper -->
-    
         </div>
     </div>
-</div>
 
-<br />
-<br />
-<br />
-
+    <br />
+    <br />
+    <br />
 </template>
 
 <script>
@@ -121,29 +113,29 @@ export default {
                 phone: '',
                 description: ''
             }
-            
+
         }
     },
-        computed : {
-            ...mapState('contactModule', ['contacts']),
-         ...mapGetters('contactModule',['numberOfContacts'])
-         },
+    computed: {
+        ...mapState('contactModule', ['contacts']),
+        ...mapGetters('contactModule', ['numberOfContacts'])
+    },
 
-          created() {
-            this.$store.dispatch('contactModule/getContact');
-        },
+    created() {
+        this.$store.dispatch('contactModule/getContact');
+    },
     methods: {
         handleImageChange() {
             this.image = this.$refs.imageInput.files[0];
         },
-        async handleAddNews(){
-            try{
+        async handleAddNews() {
+            try {
                 await this.$store.dispatch('contactModule/addContact', this.contact);
                 this.$router.push('/')
-            }catch(err){
+            } catch (err) {
                 console.log('error ---- ', err.message);
             }
-            
+
         }
     }
 }
@@ -154,151 +146,17 @@ export default {
 
 
 <style scoped>
-
-body{margin-top:20px;}
-.footer_area {
-    position: relative;
-    z-index: 1;
-    overflow: hidden;
-webkit-box-shadow: 0 8px 48px 8px rgba(47, 91, 234, 0.175);
-    box-shadow: 0 8px 48px 8px rgba(47, 91, 234, 0.175);
-    padding:60px;
-}
-.footer_area .row {
-    margin-left: -25px;
-    margin-right: -25px;
-}
-.footer_area .row .col,
-.footer_area .row .col-1,
-.footer_area .row .col-10,
-.footer_area .row .col-11,
-.footer_area .row .col-12,
-.footer_area .row .col-2,
-.footer_area .row .col-3,
-.footer_area .row .col-4,
-.footer_area .row .col-5,
-.footer_area .row .col-6,
-.footer_area .row .col-7,
-.footer_area .row .col-8,
-.footer_area .row .col-9,
-.footer_area .row .col-auto,
-.footer_area .row .col-lg,
-.footer_area .row .col-lg-1,
-.footer_area .row .col-lg-10,
-.footer_area .row .col-lg-11,
-.footer_area .row .col-lg-12,
-.footer_area .row .col-lg-2,
-.footer_area .row .col-lg-3,
-.footer_area .row .col-lg-4,
-.footer_area .row .col-lg-5,
-.footer_area .row .col-lg-6,
-.footer_area .row .col-lg-7,
-.footer_area .row .col-lg-8,
-.footer_area .row .col-lg-9,
-.footer_area .row .col-lg-auto,
-.footer_area .row .col-md,
-.footer_area .row .col-md-1,
-.footer_area .row .col-md-10,
-.footer_area .row .col-md-11,
-.footer_area .row .col-md-12,
-.footer_area .row .col-md-2,
-.footer_area .row .col-md-3,
-.footer_area .row .col-md-4,
-.footer_area .row .col-md-5,
-.footer_area .row .col-md-6,
-.footer_area .row .col-md-7,
-.footer_area .row .col-md-8,
-.footer_area .row .col-md-9,
-.footer_area .row .col-md-auto,
-.footer_area .row .col-sm,
-.footer_area .row .col-sm-1,
-.footer_area .row .col-sm-10,
-.footer_area .row .col-sm-11,
-.footer_area .row .col-sm-12,
-.footer_area .row .col-sm-2,
-.footer_area .row .col-sm-3,
-.footer_area .row .col-sm-4,
-.footer_area .row .col-sm-5,
-.footer_area .row .col-sm-6,
-.footer_area .row .col-sm-7,
-.footer_area .row .col-sm-8,
-.footer_area .row .col-sm-9,
-.footer_area .row .col-sm-auto,
-.footer_area .row .col-xl,
-.footer_area .row .col-xl-1,
-.footer_area .row .col-xl-10,
-.footer_area .row .col-xl-11,
-.footer_area .row .col-xl-12,
-.footer_area .row .col-xl-2,
-.footer_area .row .col-xl-3,
-.footer_area .row .col-xl-4,
-.footer_area .row .col-xl-5,
-.footer_area .row .col-xl-6,
-.footer_area .row .col-xl-7,
-.footer_area .row .col-xl-8,
-.footer_area .row .col-xl-9,
-.footer_area .row .col-xl-auto {
-    padding-right: 25px;
-    padding-left: 25px;
+body {
+    margin-top: 20px;
 }
 
-.single-footer-widget {
-    position: relative;
-    z-index: 1;
-}
-.single-footer-widget .copywrite-text a {
-    color: #747794;
-    font-size: 1rem;
-}
-.single-footer-widget .copywrite-text a:hover,
-.single-footer-widget .copywrite-text a:focus {
-    color: #3f43fd;
-}
-.single-footer-widget .widget-title {
-    margin-bottom: 1.5rem;
-}
-.single-footer-widget .footer_menu li a {
-    color: #747794;
-    margin-bottom: 1rem;
-    display: block;
-    font-size: 1rem;
-}
-.single-footer-widget .footer_menu li a:hover,
-.single-footer-widget .footer_menu li a:focus {
-    color: #3f43fd;
-}
-.single-footer-widget .footer_menu li:last-child a {
-    margin-bottom: 0;
-}
 
-.footer_social_area {
-    position: relative;
-    z-index: 1;
-}
-.footer_social_area a {
-    border-radius: 50%;
-    height: 40px;
-    text-align: center;
-    width: 40px;
-    display: inline-block;
-    background-color: #f5f5ff;
-    line-height: 40px;
-    -webkit-box-shadow: none;
-    box-shadow: none;
-    margin-right: 10px;
-}
-.footer_social_area a i {
-    line-height: 36px;
-}
-.footer_social_area a:hover,
-.footer_social_area a:focus {
-    color: #ffffff;
-}
 
 @-webkit-keyframes bi-cycle {
     0% {
         left: 0;
     }
+
     100% {
         left: 100%;
     }
@@ -308,16 +166,19 @@ webkit-box-shadow: 0 8px 48px 8px rgba(47, 91, 234, 0.175);
     0% {
         left: 0;
     }
+
     100% {
         left: 100%;
     }
 }
-ol li, ul li {
+
+ol li,
+ul li {
     list-style: none;
 }
 
-ol, ul {
+ol,
+ul {
     margin: 0;
     padding: 0;
-}
-</style>
+}</style>
